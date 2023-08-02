0745e26f19fbe6c37e6400f7e672d8761291e08a
azubin <alexmzubin@gmail.com>
2023-08-02 17:54:37 +0530

    Added entity

---
+++ src/main/java/com/springdatajpa/springboot/SpringDataJpaCourseApplication.java
@@ -0,0 +1,14 @@
+package com.springdatajpa.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaCourseApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the app");
+		SpringApplication.run(SpringDataJpaCourseApplication.class, args);
+
+	}
+}
